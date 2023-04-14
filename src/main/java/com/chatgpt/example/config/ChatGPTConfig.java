dc138949315632fae275948ad13be95a4a682268
kimueibin <at365@naver.com>
2023-04-15 00:17:54 +0900

    setting : setting gpt config

---
+++ src/main/java/com/chatgpt/example/config/ChatGPTConfig.java
@@ -0,0 +1,26 @@
+package com.chatgpt.example.config;
+
+import com.theokanning.openai.service.OpenAiService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/*
+ * Chat GPT 라이브러리를 사용하기전, 해당 서비스에 토큰 주입을 하기위한 Config
+ * API Key 발급은 https://platform.openai.com/account/api-keys 에서 발급받을 수 있습니다.
+ */
+@Slf4j
+@Configuration
+public class ChatGPTConfig {
+
+  @Value("${gpt.token}")
+  private String token;
+
+  @Bean
+  public OpenAiService openAiService() {
+    log.info("token : {}을 활용한 OpenAiService 을 생성합니다.", token);
+    return new OpenAiService(token);
+  }
+
+}
