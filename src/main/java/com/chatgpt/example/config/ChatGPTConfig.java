060e17ec9f045e5bd73de2dba2c6dd4c2658a184
kimueibin <at365@naver.com>
2023-04-15 02:01:33 +0900

    modify : chat gpt config 내 wait time 추가

--- src/main/java/com/chatgpt/example/config/ChatGPTConfig.java
+++ src/main/java/com/chatgpt/example/config/ChatGPTConfig.java
@@ -1,6 +1,7 @@
 package com.chatgpt.example.config;
 
 import com.theokanning.openai.service.OpenAiService;
+import java.time.Duration;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -20,7 +21,7 @@ public class ChatGPTConfig {
   @Bean
   public OpenAiService openAiService() {
     log.info("token : {}을 활용한 OpenAiService 을 생성합니다.", token);
-    return new OpenAiService(token);
+    return new OpenAiService(token, Duration.ofSeconds(60));
   }
 
 }
