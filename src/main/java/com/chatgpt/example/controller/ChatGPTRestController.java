fbda06e00a2f6af78477c270be145d76e5202968
kimueibin <at365@naver.com>
2023-04-15 02:02:27 +0900

    feat : Chat GPT REST API 추가

---
+++ src/main/java/com/chatgpt/example/controller/ChatGPTRestController.java
@@ -0,0 +1,33 @@
+package com.chatgpt.example.controller;
+
+import com.chatgpt.example.dto.request.GPTCompletionChatRequest;
+import com.chatgpt.example.dto.response.CompletionChatResponse;
+import com.chatgpt.example.dto.response.CompletionResponse;
+import com.chatgpt.example.dto.request.GPTCompletionRequest;
+import com.chatgpt.example.service.GPTChatRestService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/chatgpt/rest")
+@RequiredArgsConstructor
+public class ChatGPTRestController {
+
+  private final GPTChatRestService gptChatRestService;
+
+  @PostMapping("/completion")
+  public CompletionResponse completion(@RequestBody GPTCompletionRequest gptCompletionRequest) {
+
+    return gptChatRestService.completion(gptCompletionRequest);
+  }
+
+  @PostMapping("/completion/chat")
+  public CompletionChatResponse completionChat(@RequestBody GPTCompletionChatRequest gptCompletionChatRequest) {
+
+    return gptChatRestService.completionChat(gptCompletionChatRequest);
+  }
+
+}
---
+++ src/main/java/com/chatgpt/example/service/GPTChatRestService.java
@@ -0,0 +1,81 @@
+package com.chatgpt.example.service;
+
+import com.chatgpt.example.dto.request.GPTCompletionChatRequest;
+import com.chatgpt.example.dto.response.CompletionChatResponse;
+import com.chatgpt.example.dto.response.CompletionResponse;
+import com.chatgpt.example.dto.response.CompletionResponse.Message;
+import com.chatgpt.example.dto.request.GPTCompletionRequest;
+import com.chatgpt.example.entity.answer.GPTAnswer;
+import com.chatgpt.example.entity.answer.GPTAnswerRepository;
+import com.chatgpt.example.entity.question.GPTQuestion;
+import com.chatgpt.example.entity.question.GPTQuestionRepository;
+import com.theokanning.openai.completion.CompletionResult;
+import com.theokanning.openai.completion.chat.ChatCompletionResult;
+import com.theokanning.openai.service.OpenAiService;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class GPTChatRestService {
+  private final OpenAiService openAiService;
+
+  private final GPTQuestionRepository questionRepository;
+
+  private final GPTAnswerRepository answerRepository;
+
+  @Transactional
+  public CompletionResponse completion(final GPTCompletionRequest restRequest) {
+    CompletionResult result = openAiService.createCompletion(GPTCompletionRequest.of(restRequest));
+    CompletionResponse response = CompletionResponse.of(result);
+
+    List<String> messages = response.getMessages().stream()
+        .map(Message::getText)
+        .collect(Collectors.toList());
+
+    GPTAnswer gptAnswer = saveAnswer(messages);
+    saveQuestion(restRequest.getPrompt(), gptAnswer);
+
+    return response;
+  }
+
+  @Transactional
+  public CompletionChatResponse completionChat(GPTCompletionChatRequest gptCompletionChatRequest) {
+    ChatCompletionResult chatCompletion = openAiService.createChatCompletion(
+        GPTCompletionChatRequest.of(gptCompletionChatRequest));
+
+    CompletionChatResponse response = CompletionChatResponse.of(chatCompletion);
+
+    List<String> messages = response.getMessages().stream()
+        .map(CompletionChatResponse.Message::getMessage)
+        .collect(Collectors.toList());
+
+    GPTAnswer gptAnswer = saveAnswer(messages);
+
+    saveQuestion(gptCompletionChatRequest.getMessage(), gptAnswer);
+
+    return response;
+  }
+
+
+
+  private void saveQuestion(String question, GPTAnswer answer) {
+    GPTQuestion questionEntity = new GPTQuestion(question, answer);
+    questionRepository.save(questionEntity);
+  }
+
+  private GPTAnswer saveAnswer(List<String> response) {
+
+    String answer = response.stream()
+        .filter(Objects::nonNull)
+        .collect(Collectors.joining());
+
+    return answerRepository.save(new GPTAnswer(answer));
+  }
+
+}
